fc03fdc858295dcd73292df3813b9cb3d0f97a73
Persicjr <122411940+Persicjr@users.noreply.github.com>
2023-07-05 07:42:05 -0400

    Update join-ad-default-script.ps1

--- Computer/join-ad-default-script.ps1
+++ Computer/join-ad-default-script.ps1
@@ -3,5 +3,3 @@
 $joinCred = New-Object System.Management.Automation.PSCredential ("RangeControl",$(ConvertTo-SecureString -AsPlainText -Force "R@ng3P@`$`$w0rd!!"))
 
 Add-Computer -Domain "cityofmittelberg.gov" -Credential $joinCred -Force
-
-shutdown /r
