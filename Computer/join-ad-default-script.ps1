0f141df0e248a7038e2db453bafe8b4680fde4e0
Persicjr <122411940+Persicjr@users.noreply.github.com>
2023-07-11 22:47:01 -0400

    Update join-ad-default-script.ps1

--- Computer/join-ad-default-script.ps1
+++ Computer/join-ad-default-script.ps1
@@ -4,4 +4,13 @@ $joinCred = New-Object System.Management.Automation.PSCredential ("RangeControl"
 
 Add-Computer -Domain "cityofmittelberg.gov" -Credential $joinCred -Force
 
+# Disable the Windows Firewall in the registry
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\SharedAccess\Parameters\FirewallPolicy\StandardProfile" -Name "EnableFirewall" -Value 0
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\SharedAccess\Parameters\FirewallPolicy\PublicProfile" -Name "EnableFirewall" -Value 0
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\SharedAccess\Parameters\FirewallPolicy\DomainProfile" -Name "EnableFirewall" -Value 0
+
+# Verify the status of the Windows Firewall
+$firewallStatus = Get-NetFirewallProfile | Select-Object Name, Enabled
+$firewallStatus
+
 shutdown /r
